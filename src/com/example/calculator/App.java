639f9bd07731cc4ad23b868236a26703dfe76e52
Kang Dal Ho <d4lh0w@naver.com>
2025-02-26 14:07:17 +0900

    계산기 수정

--- src/com/example/calculator/App.java
+++ src/com/example/calculator/App.java
@@ -5,36 +5,19 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-
-
         Scanner scanner = new Scanner(System.in);
-        while (true) { //계속 반복
-
+        while (true){ //계속반복
             System.out.println("첫 번째 숫자를 입력하세요: ");
-            int a1 = scanner.nextInt();
+            double a1 = scanner.nextDouble();
+
             System.out.println("두 번째 숫자를 입력하세요: ");
-            int a2 = scanner.nextInt();
+            double a2 = scanner.nextDouble();
+
             System.out.println("사칙연산 기호를 입력하세요: ");
             char b1 = scanner.next().charAt(0);
 
-            int sum = 0; // 미리 선언 -> {}내부에서 선언된 변수는 해당 블록을 벗어나면 사용할 수 없음.
-            if (b1 == '+') {
-                sum = a1 + a2;
-            } else if (b1 == '-') {
-                sum = a1 - a2;
-            } else if (b1 == '*') {
-                sum = a1 * a2;
-            } else if (b1 == '/') {
-                if (a2 == 0) {
-                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                    continue; //0 입력시 다시 처음부터 반복
-                }
-                sum = a1 / a2;
-            } else {
-                System.out.println("잘못된 연산자입니다.");
-                continue; //
-            }
-            System.out.println("값:" + sum);
+            double result = calculator.calculate(a1, a2, b1);
+            System.out.println("값 = " + result);
 
             System.out.println("더 계산하시겠습니까? (exit 입력시 종료)");
             String answer = scanner.next();
--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -1,25 +1,25 @@
 package com.example.calculator;
 
 public class Calculator {
-
-    // 메서드 선언
-    int sum(int a1, int b1) {
-            int result = a1 + b1;
-            return result;
-    }
-    int sub(int a1, int b1) {
-            int result = a1 - b1;
-            return result;
-    }
-    int mul(int a1, int b1) {
-        int result = a1 * b1;
-        return result;
-    }
-    int div(int a1, int b1) {
-        int result = a1 / b1;
-        return result;
-    }
-
-
-
+    public double calculate(double a1, double a2, char b1) {
+        double result = 0;
+        if (b1 == '+') {
+            result = a1 + a2;
+        } else if (b1 == '-') {
+            result = a1 - a2;
+        } else if (b1 == '*') {
+            result = a1 * a2;
+        } else if (b1 == '/') {
+            if (a2 == 0) {
+                System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                return 0;
+            }
+            result = (double) a1 / a2;
+        } else {
+            System.out.println("잘못된 연산자입니다.");
+            return 0;
+        }
+    return result;}
 }
+
+
