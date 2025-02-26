2cf6b2dbc9e1db6f90a09182ba34b2b1187fc40d
Kang Dal Ho <d4lh0w@naver.com>
2025-02-26 11:49:18 +0900

    계산기

---
+++ src/com/example/calculator/Calculator.java
@@ -0,0 +1,25 @@
+package com.example.calculator;
+
+public class Calculator {
+
+    // 메서드 선언
+    int sum(int a1, int b1) {
+            int result = a1 + b1;
+            return result;
+    }
+    int sub(int a1, int b1) {
+            int result = a1 - b1;
+            return result;
+    }
+    int mul(int a1, int b1) {
+        int result = a1 * b1;
+        return result;
+    }
+    int div(int a1, int b1) {
+        int result = a1 / b1;
+        return result;
+    }
+
+
+
+}
